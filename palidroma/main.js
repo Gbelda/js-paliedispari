23c5e4f0371201658a82742cf4df9fc01a30cd34
gbelda <gbelda56@gmail.com>
2021-10-19 15:17:54 +0200

    Complete palindrome function

--- palidroma/main.js
+++ palidroma/main.js
@@ -7,3 +7,21 @@
 //reverse
 //join
 
+let userWord = prompt("Type in a word")
+
+/**
+ * Check if word is a palindrome
+ * @param {string} text - word to be checked
+ */
+function palindrome(text) {
+    let insertedWord = text.toLowerCase();
+    let reverseWord = insertedWord.split('').reverse().join('');
+    return insertedWord == reverseWord
+}
+
+//creare regola di controllo e stampa risultato
+if (palindrome(userWord)) {
+    console.log("The word inserted is a palindrome");
+} else {
+    console.log('Not a palindrome');
+}
