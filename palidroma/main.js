7f83305c11dc3c38427c613696e9e1656b5e5094
gbelda <gbelda56@gmail.com>
2021-10-19 14:15:50 +0200

    Create HTML and JS structure

---
+++ palidroma/main.js
@@ -0,0 +1,9 @@
+//Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+//Strumenti da utilizzare:
+// prompt
+//function
+//split
+//reverse
+//join
+
